f6888c13df5ba9a5d3e127fe8f94092827a2a258
Haris Beha <harisbeha@gmail.com>
2017-11-19 11:07:10 -0500

    Create AttendeeList component

---
+++ src/components/AttendeeList.js
@@ -0,0 +1,24 @@
+import React from "react";
+import PropTypes from "prop-types";
+import Attendee from "../components/Attendee";
+
+const AttendeeList = ({ attendees, time }) => {
+  return (
+    <div>
+      <h5>In Attendance</h5>
+      <ul className="list-group attendee-list">
+        {attendees.map(attendee => (
+          <li key={attendee.id} className="list-group-item list-group-item-action attendee-list__item">
+            <Attendee attendee={attendee} />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+AttendeeList.propTypes = {
+  attendees: PropTypes.array.isRequired
+};
+
+export default AttendeeList
